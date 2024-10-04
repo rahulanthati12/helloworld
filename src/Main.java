eaa282d8bcebf592b9efcfd89c485301c36b606a
rahul goud <rahulgoud3235@gmail.com>
2024-10-03 22:38:54 -0400

    main.java updated with new message 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rahul says Hello world!");
+        System.out.println("Rahul1 says Hello world!");
     }
 }
